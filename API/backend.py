7169c91096a48ef36d256c3cd7a97dd648d5c52b
Muhammad Alfian Pratama <alfianp613@gmail.com>
2023-06-09 22:01:23 +0700

    Finalisasi Model + Nyicil API

--- API/backend.py
+++ API/backend.py
@@ -1,4 +1,23 @@
 # Import Library
+import pandas as pd
+
+
+import PyPDF2
+
+## Firebase
+import firebase_admin
+from firebase_admin import credentials, firestore, storage
+
+## Model
+import tensorflow as tf
+from transformers import TFAutoModelForSeq2SeqLM, AutoTokenizer
+
+
+hg_token = "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"
+cred = firebase_admin.credentials.Certificate("talentease-project-firebase-adminsdk-db1kq-66426f47e0.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()  # this connects to our Firestore database
+
 
 # Get PDF CV Data
 
@@ -7,3 +26,12 @@
 
 
 # Summary model predict
+def summary_pred(text, id):
+    model = TFAutoModelForSeq2SeqLM.from_pretrained('walkerrose/cv_summarization',use_auth_token=hg_token)
+    tokenizer = AutoTokenizer.from_pretrained('walkerrose/cv_summarization',use_auth_token=hg_token)
+    pred_token = tokenizer(text, max_length=512, padding="max_length", return_tensors="np").input_ids
+    print("Generate Summary...")
+    outputs = model.generate(pred_token,min_length=50, max_length=150,early_stopping=True)
+    hasil = {"summary": tokenizer.decode(outputs[0], skip_special_tokens=True)}
+    db.collection('ml test').document(id).set(hasil)
+    print("Data berhasil disimpan di database")
--- API/main.py
+++ API/main.py
@@ -2,16 +2,15 @@ from flask import Flask,request, jsonify, abort, send_file
 from flask_cors import CORS
 import json
 
-# import firebase_admin
-# from firebase_admin import credentials, firestore, storage
+import firebase_admin
+from firebase_admin import credentials, firestore, storage
 
 from datetime import datetime, timedelta
 from backend import *
 
-### Connect Firebase
-# cred = firebase_admin.credentials.Certificate("project-nlp-9b41d-firebase-adminsdk-w4jxt-038c435e97.json")
-# firebase_admin.initialize_app(cred,{
-#     'storageBucket': 'project-nlp-9b41d.appspot.com'},name='bicarapilpres')
+# # Connect Firebase
+# cred = firebase_admin.credentials.Certificate("talentease-project-firebase-adminsdk-db1kq-66426f47e0.json")
+# firebase_admin.initialize_app(cred)
 # db = firestore.client()  # this connects to our Firestore database
 
 
@@ -27,26 +26,30 @@ def home():
 
 @app.route('/api/extract', methods=['POST'])
 def extract():
-    
+    if not request.json:
+        abort(400)
+    data = request.get_json()
 
 @app.route('/api/summary', methods=['POST'])
 def summary():
-    
-
-@app.route('/api/wordcloudsent', methods=['POST'])
-def get_wordcloudsent():
-    if not request.json or not 'status' in request.json:
+    if not request.json:
         abort(400)
-    data = request.get_json()
-    if data['status'] != 'minta datanya dong':
-            abort(400)
     else:
-        credentials = service_account.Credentials.from_service_account_file("project-nlp-9b41d-firebase-adminsdk-w4jxt-038c435e97.json")
-        calon = data['calon']
-        waktu = data['waktu']
-        sent = data['sentiment']
-        storage.Client(credentials=credentials).bucket(firebase_admin.storage.bucket().name).blob(f'wordcloud/{calon}/wordcloud_mention_{calon}_{waktu}_{sent}.jpg').download_to_filename(f'wordcloud/{calon}/wordcloud_mention_{calon}_{waktu}_{sent}.jpg')
-        return send_file(f'wordcloud/{calon}/wordcloud_mention_{calon}_{waktu}_{sent}.jpg', mimetype=f'image/png')
+        data = request.get_json()
+        dbs = db.collection('ml test')
+        doc = dbs.document(data['id'])
+        res = doc.get().to_dict()
+        return jsonify(res),201
+
+@app.route('/api/upload', methods=['POST'])
+def upload():
+    if not request.json:
+        abort(400)
+    else:
+        data = request.get_json()
+        summary_pred(data['cv'],data['id'])
+        return jsonify({"response":"Hasil prediksi berhasil masuk ke database!"}),201
+
 
 if __name__ == '__main__':
     app.run(debug=True)
