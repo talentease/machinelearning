d6c713529af3efe7ac6d0675d68297efd1db07cb
Muhammad Alfian Pratama <alfianp613@gmail.com>
2023-06-14 17:14:31 +0700

    Update main.py

--- API/main.py
+++ API/main.py
@@ -1,6 +1,7 @@
 from flask import Flask,request, jsonify, abort, send_file
 from flask_cors import CORS
 from backend import *
+import os
 
 # # Connect Firebase
 # cred = firebase_admin.credentials.Certificate("talentease-project-firebase-adminsdk-db1kq-66426f47e0.json")
@@ -35,4 +36,4 @@ def upload():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT",8080)), debug=True)
